e4005d2e486e7b95f1b566a115a5ecec4d0c7326
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-16 16:16:12 -0500

    Add methods for SongsRepository.kt.

--- app/src/main/java/com/example/musicman/repository/SongsRepository.kt
+++ app/src/main/java/com/example/musicman/repository/SongsRepository.kt
@@ -1,4 +1,11 @@
 package com.example.musicman.repository
 
+import android.net.Uri
+import com.example.musicman.model.Song
+
 interface SongsRepository {
+    fun getSongsIds(): List<String>
+    fun getSong(id: String): Song?
+    fun getSongs(): List<Song>
+    fun getSongUri(id: String): Uri?
 }
