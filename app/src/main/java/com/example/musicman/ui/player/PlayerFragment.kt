e9af78b74b68c14c68418ada385034786e1c73c2
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-16 16:21:02 -0500

    Show nothing in player for now.

--- app/src/main/java/com/example/musicman/ui/player/PlayerFragment.kt
+++ app/src/main/java/com/example/musicman/ui/player/PlayerFragment.kt
@@ -1,5 +1,8 @@
 package com.example.musicman.ui.player
 
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.musicman.R
@@ -11,5 +14,8 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
     private val detailsViewModel by viewModels<PlayerViewModel>()
     private val binding by viewBinding(FragmentPlayerBinding::bind)
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.nothingPlaying.visibility = View.VISIBLE
+        binding.playerGroup.visibility = View.GONE
+    }
 }
