9ff11f721abbc45745f1a63d7d56f15ce48afad6
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-16 16:15:44 -0500

    Add more info and bitmap for Song.

--- app/src/main/java/com/example/musicman/model/Song.kt
+++ app/src/main/java/com/example/musicman/model/Song.kt
@@ -1,9 +1,17 @@
 package com.example.musicman.model
 
+import android.graphics.Bitmap
+
 data class Song(
     val id: String,
-    val title: String,
-    val album: String,
-    val artist: String,
-    val albumArtist: String,
-)
+    val title: String?,
+    val album: String?,
+    val artist: String?,
+    val albumArtist: String?,
+    val track: Int?,
+    val disk: Int?,
+    val artworkBitmap: Bitmap?
+) {
+    val effectiveArtist: String?
+        get() = albumArtist ?: artist
+}
