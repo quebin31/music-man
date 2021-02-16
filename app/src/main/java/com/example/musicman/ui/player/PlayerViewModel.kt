4e7c4fbeae0bb5fe987c5657b5c27b2810d86c8c
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-16 17:55:24 -0500

    Add helper fields to get and set current song on PlayerViewModel.kt.

--- app/src/main/java/com/example/musicman/ui/player/PlayerViewModel.kt
+++ app/src/main/java/com/example/musicman/ui/player/PlayerViewModel.kt
@@ -3,12 +3,16 @@ package com.example.musicman.ui.player
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.musicman.model.Song
+import com.example.musicman.repository.SongsRepository
 
 class PlayerViewModel : ViewModel() {
+    lateinit var repository: SongsRepository
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+    val currentSong: LiveData<Song?>
+        get() = repository.getCurrentSong()
+
+    fun setCurrentSong(song: Song) {
+        repository.setCurrentSong(song)
     }
-
-    val text: LiveData<String> = _text
 }
