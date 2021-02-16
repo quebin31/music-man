ab14a32ce6cbe3befc32e9e7a840e14b14401b9c
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-15 19:38:40 -0500

    Refactor ui initialization on MainActivity.kt.

--- app/src/main/java/com/example/musicman/MainActivity.kt
+++ app/src/main/java/com/example/musicman/MainActivity.kt
@@ -1,26 +1,38 @@
 package com.example.musicman
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.musicman.databinding.ActivityMainBinding
+import com.example.musicman.extensions.viewBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val navHostFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+    }
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
+    private val navController get() = navHostFragment.navController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        setContentView(binding.root)
+        setupUi()
+    }
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+    private fun setupUi() {
+        fun setupNavUi() {
+            val appBarConfiguration = AppBarConfiguration(setOf(
+                    R.id.navigationSongs, R.id.navigationPlayer))
+
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            binding.navView.setupWithNavController(navController)
+        }
+
+        setupNavUi()
     }
 }
