505d70b18710b5cf972b78c15f7a84fe734386dc
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-17 16:55:04 -0500

    Add helper method to create MediaMetadataCompat from Song.

--- app/src/main/java/com/example/musicman/model/Song.kt
+++ app/src/main/java/com/example/musicman/model/Song.kt
@@ -1,6 +1,10 @@
 package com.example.musicman.model
 
+import android.content.Context
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.support.v4.media.MediaMetadataCompat
+import com.example.musicman.R
 
 data class Song(
     val id: String,
@@ -14,4 +18,21 @@ data class Song(
 ) {
     val effectiveArtist: String?
         get() = albumArtist ?: artist
+
+    fun asMediaMetadata(context: Context): MediaMetadataCompat {
+        val unknown = context.getString(R.string.unknown_string)
+        val defaultArtwork =
+            BitmapFactory.decodeResource(context.resources, R.drawable.default_album_artwork)
+
+        return MediaMetadataCompat.Builder()
+            .putString(MediaMetadataCompat.METADATA_KEY_TITLE, title ?: unknown)
+            .putString(MediaMetadataCompat.METADATA_KEY_DISPLAY_TITLE, title ?: unknown)
+            .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, artist ?: unknown)
+            .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, album ?: unknown)
+            .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ARTIST, albumArtist ?: unknown)
+            .putLong(MediaMetadataCompat.METADATA_KEY_TRACK_NUMBER, track?.toLong() ?: 0L)
+            .putLong(MediaMetadataCompat.METADATA_KEY_DISC_NUMBER, disk?.toLong() ?: 0L)
+            .putBitmap(MediaMetadataCompat.METADATA_KEY_ALBUM_ART, artworkBitmap ?: defaultArtwork)
+            .build()
+    }
 }
