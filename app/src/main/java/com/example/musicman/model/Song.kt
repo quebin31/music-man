62f5e25860e9b6a3a88d4e4a37a31bbe64b7ca89
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-18 18:34:35 -0500

    Include uri on song data class.

--- app/src/main/java/com/example/musicman/model/Song.kt
+++ app/src/main/java/com/example/musicman/model/Song.kt
@@ -3,11 +3,13 @@ package com.example.musicman.model
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.net.Uri
 import android.support.v4.media.MediaMetadataCompat
 import com.example.musicman.R
 
 data class Song(
     val id: String,
+    val uri: Uri,
     val title: String?,
     val album: String?,
     val artist: String?,
@@ -16,8 +18,7 @@ data class Song(
     val disk: Int?,
     val artworkBitmap: Bitmap?
 ) {
-    val effectiveArtist: String?
-        get() = albumArtist ?: artist
+    val effectiveArtist: String? get() = albumArtist ?: artist
 
     fun asMediaMetadata(context: Context): MediaMetadataCompat {
         val unknown = context.getString(R.string.unknown_string)
