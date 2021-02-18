f10ff961900bfe0fb5c93a71b602226895e126e1
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-18 18:35:01 -0500

    Update SongsRepository to use live data, remove uri getter.

--- app/src/main/java/com/example/musicman/repository/SongsRepository.kt
+++ app/src/main/java/com/example/musicman/repository/SongsRepository.kt
@@ -1,14 +1,11 @@
 package com.example.musicman.repository
 
-import android.net.Uri
 import androidx.lifecycle.LiveData
 import com.example.musicman.model.Song
 
 interface SongsRepository {
-    fun getSongsIds(): List<String>
-    fun getSong(id: String): Song?
-    fun getSongs(): List<Song>
-    fun getSongUri(id: String): Uri?
+    fun getSongs(): LiveData<List<Song>>
+    fun getSong(id: String): LiveData<Song?>
     fun getLatestPlayedSong(): LiveData<Song?>
     fun setLatestPlayedSong(song: Song)
 }
