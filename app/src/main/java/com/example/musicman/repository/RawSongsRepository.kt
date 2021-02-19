369a0fbbe034a46e57498a1822741a9531653135
Kevin Del Castillo <kevindelcastillo@ravn.co>
2021-02-19 17:08:13 -0500

    Refactor RawSongsRepository implementation.

--- app/src/main/java/com/example/musicman/repository/RawSongsRepository.kt
+++ app/src/main/java/com/example/musicman/repository/RawSongsRepository.kt
@@ -1,42 +1,44 @@
 package com.example.musicman.repository
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import android.media.MediaMetadataRetriever
-import android.util.Log
-import androidx.core.content.edit
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.createDataStore
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
 import com.example.musicman.R
 import com.example.musicman.extensions.*
 import com.example.musicman.model.Song
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RawSongsRepository @Inject constructor(@ApplicationContext private val context: Context) :
     SongsRepository {
 
-    override fun getSongs(): LiveData<List<Song>> = liveData {
-        Log.i("Repo", "getSongs: getting songs")
-        emit(listOf("raw1", "raw2", "raw3").mapNotNull { buildSong(it) })
-    }
+    private val dataStore: DataStore<Preferences> = context.createDataStore(STORE_NAME)
 
-    override fun getSong(id: String) = buildSong(id)
+    override val songs: LiveData<List<Song>>
+        get() = liveData {
+            emit(listOf("raw1", "raw2", "raw3").mapNotNull { buildRawSong(it) })
+        }
 
-    override fun setLatestPlayedSong(song: Song) {
-        val prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
-        prefs.edit(commit = true) {
-            putString(KEY_LATEST_SONG, song.id)
+    override val latestPlayedSong: LiveData<String?>
+        get() = dataStore.data.map { it[LATEST_SONG] }.asLiveData()
+
+    override fun get(id: String): Song? = buildRawSong(id)
+
+    override suspend fun setLatestPlayedSong(id: String) {
+        dataStore.edit { settings ->
+            settings[LATEST_SONG] = id
         }
     }
 
-    override fun getLatestPlayedSong(): LiveData<Song?> = liveData {
-        val prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
-        val song = prefs.getString(KEY_LATEST_SONG, null)?.let { buildSong(it) }
-        emit(song)
-    }
-
-    private fun buildSong(id: String): Song? {
+    private fun buildRawSong(id: String): Song? {
         val uri = when (id) {
             "raw1" -> context.getAndroidUri(R.raw.raw1)
             "raw2" -> context.getAndroidUri(R.raw.raw2)
@@ -60,7 +62,7 @@ class RawSongsRepository @Inject constructor(@ApplicationContext private val con
     }
 
     companion object {
-        private const val PREFS = "RawRepositoryPreferences"
-        private const val KEY_LATEST_SONG = "latest_song"
+        private const val STORE_NAME = "com.example.musicman.store"
+        private val LATEST_SONG = stringPreferencesKey("com.example.musicman.store.latestsong")
     }
 }
